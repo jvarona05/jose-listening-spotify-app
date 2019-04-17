5951cc8a67e1798592f49ed1e03ec58b2247b907
José <jose.varona05@gmail.com>
2019-04-17 20:33:54 +0200

    add redis connection funciton

--- api/index.js
+++ api/index.js
@@ -5,6 +5,19 @@ require('dotenv').config()
 
 const app = express()
 
+// Redis
+
+function connectToRedis() {
+  const redisClient = redis.createClient(process.env.REDIS_URL)
+  redisClient.on('connect', () => {
+    console.log('\n🎉 Redis client connected 🎉\n')
+  })
+  redisClient.on('error', err => {
+    console.error(`\n🚨 Redis client could not connect: ${err} 🚨\n`)
+  })
+  return redisClient
+}
+
 app.all('/spotify/data/:key', (req, res) => {
 	res.send('Success! 🎉\n')
 })
