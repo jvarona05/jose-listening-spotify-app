b4b40089eef8600ac3930869116087fd6b3ca4ef
José <jose.varona05@gmail.com>
2019-04-18 09:39:48 +0200

    getAccessToken

--- api/index.js
+++ api/index.js
@@ -42,6 +42,27 @@ async function callStorage(method, ...args) {
   return response
 }
 
+async function getAccessToken() {
+  const redisClient = connectToRedis()
+  const accessTokenObj = { value: await redisClient.get('access_token') }
+  if (!Boolean(accessTokenObj.value)) {
+    const refresh_token = await redisClient.get('refresh_token')
+    const {
+      data: { access_token, expires_in }
+    } = await getSpotifyToken({
+      refresh_token,
+      grant_type: 'refresh_token'
+    })
+    Object.assign(accessTokenObj, {
+      value: access_token,
+      expires: expires_in
+    })
+    callStorage(...storageArgs('access_token', { ...accessTokenObj }))
+  }
+  redisClient.quit()
+  return accessTokenObj.value
+}
+
 const getSpotifyToken = (props = {}) =>
   axios({
     method: 'post',
