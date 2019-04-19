d186a50fbbc15c091709dba273432a4e7a674aa6
José <jose.varona05@gmail.com>
2019-04-19 16:18:21 +0200

    fix store code for the spotify data

--- store/index.js
+++ store/index.js
@@ -1,72 +1,84 @@
 import axios from 'axios'
 
-const clientUrl = process.env.CLIENT_URL
-
 export const state = () => ({
-  isConnected: false,
-  message: null,
-  nowPlaying: {},
-  recentlyPlayed: {},
-  trackProgress: 0,
-  isPlaying: false
+    isConnected: false,
+    message: null,
+    nowPlaying: {},
+    recentlyPlayed: {},
+    trackProgress: 0,
+    isPlaying: false
 })
 
 export const mutations = {
-  connectionChange(state, isConnected) {
-    state.isConnected = isConnected
-  },
-  messageChange(state, message) {
-    state.message = message
-  },
-  nowPlayingChange(state, nowPlaying) {
-    state.nowPlaying = nowPlaying
-  },
-  isPlayingChange(state, isPlaying) {
-    state.isPlaying = isPlaying
-  },
-  progressChange(state, { progress, duration }) {
-    state.trackProgress = (progress / duration) * 100
-  },
-  recentlyPlayedChange(state, recentlyPlayed) {
-    state.recentlyPlayed = recentlyPlayed
-  }
+    connectionChange(state, isConnected) {
+        state.isConnected = isConnected
+    },
+    messageChange(state, message) {
+        state.message = message
+    },
+    nowPlayingChange(state, nowPlaying) {
+        state.nowPlaying = nowPlaying
+    },
+    isPlayingChange(state, isPlaying) {
+        state.isPlaying = isPlaying
+    },
+    progressChange(state, {progress,duration}) {
+      // console.log(progress,duration)
+        state.trackProgress = (progress / duration) * 100
+    },
+    recentlyPlayedChange(state, recentlyPlayed) {
+        state.recentlyPlayed = recentlyPlayed
+    }
 }
 
 export const actions = {
-  async nuxtServerInit({ commit }) {
-    try {
-      const redisUrl = `${clientUrl}/api/spotify/data/`
-      const {
-        data: { is_connected }
-      } = await axios.get(`${redisUrl}is_connected`)
+    async nuxtServerInit({
+        commit
+    }) {
+        try {
+            const clientUrl = process.env.CLIENT_URL
+            const redisUrl = `${clientUrl}api/spotify/data/`
+            const {
+                data: {
+                    is_connected
+                }
+            } = await axios.get(`${redisUrl}is_connected`)
 
-      commit('connectionChange', is_connected)
+            commit('connectionChange', is_connected)
+            if (Boolean(is_connected)) {
+                const data = await axios.get(`${clientUrl}api/spotify/now-playing`)
 
-      if (Boolean(is_connected)) {
-        const {
-          data: { item, is_playing }
-        } = await axios.get(`${clientUrl}/api/spotify/now-playing`)
-        commit('nowPlayingChange', item)
-        commit('isPlayingChange', is_playing)
-      }
-    } catch (err) {
-      console.error(err)
+                commit('nowPlayingChange', data.data.item)
+                commit('isPlayingChange', data.data.is_playing)
+                commit('progressChange', data.data.progress_ms)
+            }
+        } catch (err) {
+            console.error(err)
+        }
+    },
+    updateProgress: ({commit,state}, props) => {
+        commit('progressChange', props)
+        return state.trackProgress
+    },
+    updateTrack: ({
+        commit,
+        state
+    }, nowPlaying) => {
+        commit('nowPlayingChange', nowPlaying)
+        return state.nowPlaying
+    },
+    updateStatus: ({
+        commit,
+        state
+    }, isPlaying) => {
+        commit('isPlayingChange', isPlaying)
+        return state.isPlaying
+    },
+    updateConnection: ({
+        commit,
+        state
+    }, isConnected) => {
+        commit('connectionChange', isConnected)
+        return state.isConnected
     }
-  },
-  updateProgress: ({ commit, state }, props) => {
-    commit('progressChange', props)
-    return state.trackProgress
-  },
-  updateTrack: ({ commit, state }, nowPlaying) => {
-    commit('nowPlayingChange', nowPlaying)
-    return state.nowPlaying
-  },
-  updateStatus: ({ commit, state }, isPlaying) => {
-    commit('isPlayingChange', isPlaying)
-    return state.isPlaying
-  },
-  updateConnection: ({ commit, state }, isConnected) => {
-    commit('connectionChange', isConnected)
-    return state.isConnected
-  }
 }
