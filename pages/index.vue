28a4122d6310420279312c14d9ab29350b07c145
José <jose.varona05@gmail.com>
2019-04-19 16:18:46 +0200

    add code for the index page

---
+++ pages/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <section>
+    <NowPlaying v-if="isConnected && track" :nowPlaying="track" :isPlaying="isPlaying"/>
+    <p v-if="!isConnected">
+      😭 {{ $nuxt.layout.authorName }} hasn’t connected yet. 😭
+    </p>
+  </section>
+</template>
+
+<script>
+import NowPlaying from '~/components/NowPlaying.vue'
+
+export default {
+  components: { NowPlaying },
+  computed: {
+    showTrack() {
+      return this.isConnected && this.track
+    },
+    nowPlaying() {
+      if (Boolean(Object.keys(this.$store.state.nowPlaying).length !== 0)) {
+        this.$store.dispatch('updateConnection', true)
+        return this.$store.state.nowPlaying
+      }
+      return this.$store.state.recentlyPlayed
+    },
+    track() {
+      return this.nowPlaying
+    },
+    isPlaying() {
+      return this.$store.state.isPlaying
+    },
+    isConnected() {
+      return this.$store.state.isConnected
+    }
+  }
+}
+</script>
+
+<style scoped>
+section {
+  min-width: 300px;
+  transform: translateY(-50%);
+  max-width: 750px;
+  margin: auto;
+  padding: 1em;
+}
+@media (max-width: 600px) {
+  section {
+    transform: translateY(-10%) translateX(-2vw);
+    width: 65vw;
+    min-width: 200px;
+  }
+}
+</style>
